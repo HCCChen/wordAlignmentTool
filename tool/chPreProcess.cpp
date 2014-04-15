64732125639878e9bbb7073271e695516e538441
HCCChen <ccx97u@gmail.com>
2014-04-15 15:30:52 +0800

    Fix Chinese pre-process for mulit-segment-word merge by library

--- tool/chPreProcess.cpp
+++ tool/chPreProcess.cpp
@@ -7,25 +7,34 @@
 using namespace std;
 
 int explode(char divideChar, string originalString, string* stringAry);
+/*
+	return 0: no match
+	return 1: all match
+	return 2: part match
+*/
+int checkLib(string inputStr, vector<string> &basicLib);
 
 int main(int argc, char* argv[]){
 	const string CH_WORD_LIB_PATH = "../data/knownChWord";
 	const string CH_LAW_PATH = "../data/chBaseTmp";
 	const string CH_FIX_LAW_PATH = "../data/chBaseTrain";
-	map<string, int> basicWordLib;
+//	map<string, int> basicWordLib;//<known string, using time>
+	vector<string> basicWordLib;
+	vector<int> basicWordFlag;
 	fstream fin, fout;
 	char buf[4096];
 	string tmpStr, mergeWord, wordPool[512], lawSentence, emptyStr="";
 	string punctuation[16] = {" ，"," 。"," 、"," ；"," ："," ＂"," ｛"," ｝"," 「"," 」"," 『"," 』"," （"," ）"," 　", "\r"};
-	int wordPoolSize, i,j,k;
-	unsigned int loopCount, flag;
+	int wordPoolSize, i,j,k, flag, inLineFlag;
 
 	//Load Word Lib
 	fin.open(CH_WORD_LIB_PATH.c_str(), ios::in);
 	while(!fin.eof()){
 		fin.getline(buf, 4096);
 		tmpStr.assign(buf);
-		basicWordLib[tmpStr] = 1;
+	//	basicWordLib[tmpStr] = 0;
+		basicWordLib.push_back(tmpStr);
+		basicWordFlag.push_back(0);
 	}
 	fin.close();
 	
@@ -43,22 +52,43 @@ int main(int argc, char* argv[]){
 			}
 			//Merge Sentence
 			wordPoolSize = explode(' ', lawSentence, wordPool);
-			for(loopCount = 0, lawSentence = "", flag = 0; loopCount < wordPoolSize-1; loopCount++){//for each word seg
-				flag = 0;
-				tmpStr = wordPool[loopCount] + wordPool[loopCount+1];
-				if(basicWordLib[tmpStr] != 1){//Not Find it!!
-					tmpStr = wordPool[loopCount] + ' ';
+			for(i = 0, lawSentence = "",tmpStr = "", inLineFlag = -1; i < wordPoolSize; i++){//for each word seg
+				tmpStr += wordPool[i];
+				//Check known word lib
+				flag = checkLib(tmpStr, basicWordLib);
+				switch(flag){
+					case 0://No match->output
+						if(inLineFlag == -1){//Without Buffer->output
+							lawSentence += tmpStr + ' ';
+							tmpStr = "";
+							inLineFlag = -1;				
+						}
+						else{//Have Buffer-> back to first seg
+							i = inLineFlag;
+							lawSentence += wordPool[i] + ' ';
+							tmpStr = "";
+							inLineFlag = -1;				
+						}
+					break;
+					case 1://All match->Merge
+						lawSentence += tmpStr + ' ';
+						tmpStr = "";
+						inLineFlag = -1;
+					break;
+					case 2://Part match->continue check
+						if(inLineFlag == -1){
+							inLineFlag = i;//Have Buffer
+						}
+						continue;
+					break;
 				}
-				else{//Find it!
-					tmpStr = wordPool[loopCount] + wordPool[loopCount+1] + ' ';
-					loopCount++;
-					flag = 1;
+			}
+			if(inLineFlag != -1){//Have buffer -> Clear the buffer
+				for(i = inLineFlag; i < wordPoolSize; i++){
+					lawSentence += wordPool[i] + ' ';
 				}
-				lawSentence += tmpStr;
 			}
-			if(flag == 1){
-			//	lawSentence += wordPool[loopCount];
-			}
+			//Output to file
 			fout << lawSentence << endl;
 	}
 	fin.close();
@@ -79,3 +109,19 @@ int explode(char divideChar, string originalString, string* stringAry){
 	return -1;
 }
 
+int checkLib(string inputStr, vector<string> &basicLib){
+	int i, flag;
+	//Default: no match
+	for(i = 0, flag = 0; i < basicLib.size(); i++){
+		//All match
+		if(inputStr == basicLib[i]){
+			flag = 1;
+			break;
+		}
+		//Part match
+		else if(flag == 0 && basicLib[i].find(inputStr) != string::npos){
+			flag = 2;
+		}
+	}
+	return flag;
+}
